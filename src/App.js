ae3107b68fac5a5adc15aa53787edd45f92f437a
Baconwolf5986 <francisorozco12@gmail.com>
2024-11-27 19:56:50 -0500

    Changed icon, title. Added Scroll to top

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { React } from 'react';
+import { React, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -11,16 +12,28 @@ import Results from './pages/ResultsPage.js';
 import Navbar from './pages/components/Navbar.js';
 import './styles.css';
 
+// Close the button
 const ToastClose = ({ closeToast }) => (
     <button onClick={closeToast} className="custom-close-btn">
         OK
     </button>
 );
 
+const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0); // Scroll to top whenever the path changes
+    }, [pathname]);
+
+    return null; // This component doesn't render anything
+};
+
 
 function App() {
     return (
         <Router>
+        <ScrollToTop />
         <Navbar />
             <Routes>
               <Route path="/" element={<Home />} />
